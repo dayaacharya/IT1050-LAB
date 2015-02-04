b977a652238910fdde87f37165cc9346f81827b5
dayaacharya <daya_acharya@yahoo.com>
2015-02-04 16:07:49 -0500

    classes and objects

---
+++ example2/example2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            person Daya = new person();
+            person radha = new person();
+            person oldest = Daya;
+
+            Daya.FirstName = "Ram";
+            Daya.LastName = "Acharya";
+
+            radha.FirstName = "Radha";
+            radha.LastName = "Dahal";
+
+            string result1 = Daya.GetFullName();
+            string result2 = radha.GetFullName();
+
+        }
+    }
+}
---
+++ example2/example2/person.cs
@@ -0,0 +1,14 @@
+﻿
+namespace example2
+{
+    class person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+     }
+}
