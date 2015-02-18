289531c16857ea8b2ba2ad6fb9a3ddbe2d03bec0
dayaacharya <daya_acharya@yahoo.com>
2015-02-18 13:03:26 -0500

    lab 1 version 2
    lab1 version 2

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            char middleInitial;
+            string lastName;
+            string fullName;
+            int age;
+            bool iscitizen;
+            bool canvote;
+            int heightfeet;
+            double heightinches;
+            double totalheightCm;
+
+            firstName = "Daya";
+            middleInitial = 'R';
+            lastName = "Acharya";
+            fullName = firstName + middleInitial + "." + lastName;
+
+            age = 26;
+            heightfeet = 5;
+            heightinches = 8;
+            double totalinches = heightfeet * 12 + heightinches;
+            totalheightCm = 2.54 * totalinches;
+
+            iscitizen = true;
+            bool isoldenoughtovote = age >= 18;
+            canvote = iscitizen && isoldenoughtovote;
+        }
+    }
+}
---
+++ ConsoleApplication1/version2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace version2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string middleInitial;
+            string lastName;
+            string fullName;
+            int age;
+            bool isCitizen;
+            bool canVote;
+            int heightFeet;
+            double heightInches;
+            double totalHeightCm;
+
+            Console.Write("First Name     : ");
+            firstName = Console.ReadLine();
+            Console.Write("Middle Initial : ");
+            middleInitial = Console.ReadLine();
+            Console.Write("Last Name      : ");
+            lastName = Console.ReadLine();
+            fullName = firstName + " " + middleInitial + ". " + lastName;
+
+            Console.Write("Age            : ");
+            age = int.Parse(Console.ReadLine());
+            Console.Write("Height Feet    : ");
+            heightFeet = int.Parse(Console.ReadLine());
+            Console.Write("Extra Inches   : ");
+            heightInches = double.Parse(Console.ReadLine());
+            double totalInches = heightFeet * 12 + heightInches;
+            totalHeightCm = 2.54 * totalInches;
+
+            Console.Write("Citizen?       : ");
+            isCitizen = bool.Parse(Console.ReadLine());
+            bool isOldEnoughToVote = age >= 18;
+            canVote = isCitizen && isOldEnoughToVote;
+
+            Console.WriteLine();
+            Console.WriteLine("Your Full Name Is    ---> " + fullName);
+            Console.WriteLine("Your Height in CM Is ---> " + totalHeightCm);
+            Console.WriteLine("Your Voter Status Is ---> " + canVote);
+            Console.ReadKey();
+
+        }
+    }
+}
