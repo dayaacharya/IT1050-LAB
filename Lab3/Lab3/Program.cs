14e1f7873afba64e09abaf8c6d23b74c37a19182
dayaacharya <daya_acharya@yahoo.com>
2015-03-03 19:25:26 -0500

    practice lab3
    Lab3 practice

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Tell me your gender? : (M/f) ");
+            string gender = Console.ReadLine().ToLower();
+            Console.Write("Tell me your eye color? ");
+            string eyeColor = Console.ReadLine().ToLower();
+            bool isMale = gender == "m";
+            bool isFemale = gender == "f";
+
+            if (isMale && eyeColor == "blue")
+            {
+
+                Console.WriteLine("Hello" + "Joe");
+            }
+
+            if (isMale && eyeColor == "brown")
+            {
+
+                Console.WriteLine("Hello" + "Dave");
+            }
+
+            if (isMale && eyeColor == "green")
+            {
+
+                Console.WriteLine("Hello" + "John");
+            }
+
+
+            if (isFemale && eyeColor == "blue")
+            {
+
+                Console.WriteLine("Hello" + "Sally");
+            }
+
+            if (isFemale && eyeColor == "brown")
+            {
+
+                Console.WriteLine("Hello" + "Jane");
+            }
+
+            if (isFemale && eyeColor == "green")
+            {
+
+                Console.WriteLine("Hello" + "Phoebe");
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
---
+++ Lab3/part2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Tell me your gender? : (M/f) ");
+            string gender = Console.ReadLine().ToLower();
+            Console.Write("Tell me your eye color? ");
+            string eyeColor = Console.ReadLine();
+            bool isMale = gender == "m";
+            bool isFemale = gender == "f";
+
+            if (isMale && eyeColor == "blue")
+            {
+                Console.WriteLine("Hello" + "Joe");
+            }
+            
+            if (isMale && eyeColor == "Brown")
+            {
+                Console.WriteLine("Hello" + "Dave");
+            }
+            
+            if (isMale && eyeColor == "Green")
+            {
+                Console.WriteLine("Hello" + "John");
+            }
+
+            if (isFemale && eyeColor == "Blue")
+            {
+                Console.WriteLine("Hello" + "Sally");
+            }
+
+            if (isFemale && eyeColor == "Brown")
+            {
+                Console.WriteLine("Hello" + "Jane");
+            }
+            
+            if (isFemale && eyeColor == "Green")
+            {
+                Console.WriteLine("Hello" + "Phoebe");
+            }
+            
+            Console.ReadLine();
+        
+        }
+    }
+}
+    
+
