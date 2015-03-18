f8e5c17d9d5bb113f3706571cace1e48fc6683a3
dayaacharya <daya_acharya@yahoo.com>
2015-03-18 13:57:27 -0400

    changed
    lab3 edited

--- Lab3 final/Lab3/Program.cs
+++ Lab3 final/Lab3/Program.cs
@@ -7,77 +7,82 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
-           
-            
+
             Console.Write("How many children..... :");
             int childnumber = int.Parse(Console.ReadLine());
-              
-            
+
+
             Console.Write("How many adults....... :");
             int adultnumber = int.Parse(Console.ReadLine());
 
             Console.Write("How many seniors...... :");
             int seniorsnumber = int.Parse(Console.ReadLine());
 
-            Console.Write("Drink or stuff(Y/N).............:");
-            string Drink = Console.ReadLine().ToLower();            
-        
-    
-            Console.Write("Type of Drinks or stuff smallsoda,popcorn,largesoda,candy,hotdog,(y/n).....:");
-            Console.ReadLine();           
-            
+            Console.Write("Drink or stuff(y/n).............:");
+            string Drink = Console.ReadLine().ToLower();
+
+
+            Console.Write("how many smallsoda.....:");
+            int smallsodanumber = int.Parse(Console.ReadLine());
+
+            Console.Write("how many popcorn.......:");
+            int popcornnumber = int.Parse(Console.ReadLine());
+
+            Console.Write("How many hotdog.........:");
+            int hotdognumber = int.Parse(Console.ReadLine());
+
+            Console.Write("how many largesoda......:");
+            int largesodanumber = int.Parse(Console.ReadLine());
+
+            Console.Write("How many candy..........:");
+            int candynumber = int.Parse(Console.ReadLine());
 
             Console.Write("evening(y/n) :");
-            Console.ReadLine();            
+            Console.ReadLine();
 
-            bool evening = Console.ReadLine().ToLower().StartsWith("Y");            
+            bool evening = Console.ReadLine().ToLower().StartsWith("Y");
 
-            double pricetotal  = 0;
+            double pricetotal = 0;
+            double concessiontotal = 0;
+
 
             if (evening)
             {
-              if(Drink == "smallsoda"){  pricetotal = (childnumber * 6.99) + 3.50;}
-              if(Drink == "smallsoda"){pricetotal = (adultnumber * 10.99) + 3.50;}
-              if(Drink == "smallsoda"){ pricetotal = (seniorsnumber * 8.50) + 3.50;}
-
+                pricetotal = adultnumber * 10.99;
+                pricetotal = childnumber * 6.99;
+                pricetotal = seniorsnumber * 8.50;
             }
-            if (evening) 
-            {
-              if(Drink  == "largesoda"){pricetotal= (childnumber * 6.99) + 5.99;}
-              else if (Drink == "largesoda") { pricetotal = (adultnumber * 10.99) + 5.99; }
-              else if (Drink == "largesoda") { pricetotal = (seniorsnumber * 8.50) + 5.99; }
-                
-            }
-            if (evening) 
-            {
-              if(Drink == "popcorn") {pricetotal = (childnumber * 6.99) + 4.50;}
-              else if(Drink == "popcorn") {pricetotal = (adultnumber * 10.99) + 4.50;}
-              else if (Drink == "Popcorn") { pricetotal = (seniorsnumber * 8.50) + 4.50; }
-            }
+                       
             if (evening)
             {
-                if (Drink == "Candy") { pricetotal = (childnumber * 6.99) + 1.99; }
-                else if(Drink=="Candy") {pricetotal = (adultnumber * 10.99) + 1.99;}
-                else if (Drink == "Candy") { pricetotal = (seniorsnumber * 8.50) + 1.99; }
-            }
-            if (evening)
-            {
-                if (Drink == "hotdog") { pricetotal = (childnumber * 6.99) + 3.99; }
-                else if (Drink == "hotdog") { pricetotal = (adultnumber * 10.99) + 3.99;}
-                else if (Drink == "hotdog") { pricetotal = (adultnumber * 8.50) + 3.99; }
-            }
+                concessiontotal = hotdognumber * 3.99;
+                concessiontotal = smallsodanumber * 3.50;
+                concessiontotal = largesodanumber * 5.99;
+                concessiontotal = popcornnumber * 4.50;
+                concessiontotal = candynumber * 1.99;
 
+            }
             else
             {
                 pricetotal = childnumber * 3.99;
                 pricetotal = adultnumber * 5.99;
                 pricetotal = seniorsnumber * 4.50;
             }
-            Console.WriteLine(pricetotal + "." );
+
+            int discountcal = childnumber + adultnumber + seniorsnumber;
+
+            if (discountcal > 3)
+            {
+                pricetotal = pricetotal - 2; 
+            }
+
+            Console.WriteLine(pricetotal + concessiontotal + discountcal +".");
             Console.ReadKey();
 
-                    
 
-            }
+
         }
     }
+}
+    
+
